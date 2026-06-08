15dff1b360657d9e2aa8f81821f8fe11302269bd
Bijal <panchalbijal249@gmail.com>
2026-06-07 21:01:42 -0400

    Addes values to config.py

---
+++ src/config.py
@@ -0,0 +1,7 @@
+CITIES = [
+    {"name": "Toronto",  "lat": 43.6532, "lon": -79.3832},
+    {"name": "New York", "lat": 40.7128, "lon": -74.0060},
+    {"name": "London",   "lat": 51.5074, "lon": -0.1278},
+    {"name": "Mumbai",   "lat": 19.0760, "lon": 72.8777},
+    {"name": "Tokyo",    "lat": 35.6762, "lon": 139.6503},
+]
